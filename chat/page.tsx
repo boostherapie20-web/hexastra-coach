241148de93436dbe0cf224644a4e91b00d1f56d5
Hexastra <boostherapie2.0@gmail.com>
2026-03-05 16:34:19 +0100

    Update page.tsx

--- chat/page.tsx
+++ chat/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect, useRef, useCallback } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 type Message = {
   id: string
